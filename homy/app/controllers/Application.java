b37536f0dd5de8eebdc54ff1c356e34b3048cf76
olcay <o.tarazan@tum.de>
2013-06-11 16:37:10 +0200

    play version is updated to 1.2.5

--- homy/app/controllers/Application.java
+++ homy/app/controllers/Application.java
@@ -12,21 +12,5 @@ public class Application extends Controller {
     public static void index() {
         render();
     }
-    
-    public static void tasks() {
-        List tasks= Task.find("order by id desc").fetch();
-        render(tasks);
-    }
-    
-    public static void createTask(String title) {
-        Task task=new Task(title).save();
-        renderJSON(task);
-    }
-    
-    public static void changeStatus(long id, boolean done) {
-        Task task = Task.findById(id);
-        task.done=done;
-        task.save();
-        renderJSON(task);
-    }
+
 }
