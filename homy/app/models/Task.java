610193eaed8724cf71b5a0b26c91ac46805d0454
olcay <o.tarazan@tum.de>
2013-06-13 11:26:22 +0200

    task model is modified

--- homy/app/models/Task.java
+++ homy/app/models/Task.java
@@ -8,9 +8,11 @@ import java.util.*;
 
 @Entity
 public class Task extends Model {
-    public String id;
+    //public String id;
+    public String title;
     public String description;
     public boolean status;
+    public String assignee;
     
     /*better declare it as int. we can decide for predefined values,
       like 1=hourly 2=daily 3=weekly etc..*/
@@ -18,11 +20,15 @@ public class Task extends Model {
     /*better store it as a Date and then calculate remaining time*/
     public Date remainingTime;
 	
+    public Task(String title){
+	super();
+        this.title=title;
+    }
     
-    public Task(String id, String description, boolean status, int recurrence,
+    public Task(String description, String title, boolean status, int recurrence,
 			Date remainingTime) {
 		super();
-		this.id = id;
+		this.title = title;
 		this.description = description;
 		this.status = status;
 		this.recurrence = recurrence;
