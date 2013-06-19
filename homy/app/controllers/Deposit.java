1a1e96267283f22335162098b51ca39a78c73154
Ugur Aker <aliuguraker91@gmail.com>
2013-06-19 16:16:14 +0200

    tmp

--- homy/app/controllers/Deposit.java
+++ homy/app/controllers/Deposit.java
@@ -8,14 +8,33 @@ import java.util.*;
 import models.*;
 
 public class Deposit extends Controller {
+	
+	static long roomID=0;
 
-
-    public static void index(int roomId){
+    public static void index(long roomId){
+    	roomID=roomId;
     	System.out.println(roomId+" <- can be used for retrival");
     	Room currentRoom = Room.find("byName","DefaultRoom").first();
     	currentRoom.depositBox.addBoxItem(new DepositBoxItem("Something to put in the box", 11.0f, true));
+    	currentRoom.depositBox.addBoxItem(new DepositBoxItem("Something to put in the box", 12.0f, true));
     	System.out.println("the current deposit is: "+currentRoom.depositBox.currentDeposit);
-        render();
+    	List<DepositBoxItem> depositItemList=currentRoom.depositBox.depositBoxItemsList;
+        render(depositItemList);
+    }
+    public static void addItem(String User,String description,float amount,String income,String expense){
+    	Room currentRoom = Room.find("byName","DefaultRoom").first();
+    	DepositBoxItem temp=null;
+    	if(income!=null){
+    		temp=new DepositBoxItem(description,amount,true);
+    	}
+    	else if(expense!=null){
+    		temp=new DepositBoxItem(description,amount,false);
+    	}
+    	currentRoom.depositBox.addBoxItem(temp);
+    	index(roomID);
+    }
+    public static void deleteItem(int roomId){
+    	
     }
 
 }    
